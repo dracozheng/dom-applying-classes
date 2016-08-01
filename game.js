74a48f7f1429ea1e1ac31706eba99c4fbc98ec3c
Draco Zheng <641720970@qq.com>
2016-08-01 16:27:19 +1200

    change

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
-// Don't change or delete this line! It waits until the DOM has loaded, then calls 
-// the start function. More info: 
+// Don't change or delete this line! It waits until the DOM has loaded, then calls
+// the start function. More info:
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
 document.addEventListener('DOMContentLoaded', start)
 
@@ -11,7 +11,12 @@ function bindEventListeners (dots) {
   for (var i = 0; i < dots.length; i++) {
     // BIND YOUR EVENT LISTENERS HERE
     // The first one is provided for you
+    //1
     dots[i].addEventListener('contextmenu', makeGreen)
+    //2
+    dots[i].addEventListener('click', makeBlue)
+    //3
+    dots[i].addEventListener('dblclick', hide)
   }
 }
 
@@ -22,8 +27,17 @@ function makeGreen (evt) {
 }
 
 // CREATE FUNCTION makeBlue HERE
+function makeBlue(evt) {
+  evt.preventDefault()
+  evt.target.classList.toggle('blue')
+  updateCounts()
+}
 
 // CREATE FUNCTION hide HERE
+function hide (evt) {
+  evt.target.classList.add('invisible')
+  updateCounts()
+}
 
 function updateCounts () {
   var totals = {
@@ -31,8 +45,30 @@ function updateCounts () {
     green: 0,
     invisible: 0
   }
-  
+
   // WRITE CODE HERE TO COUNT BLUE, GREEN, AND INVISIBLE DOTS
+  function COUNT () {
+      return document.getElementsByClassName('board')[0].children}
+
+
+      var dotArray = COUNT()
+
+      for (var i = 0; i < dotArray.length; i++) {
+
+        if (dotArray[i].classList.contains('blue') === true){
+
+                totals.blue += 1
+        }
+        else if (dotArray[i].classList.contains('green') === true){
+
+                totals.green += 1
+        }
+        else if (dotArray[i].classList.contains('invisible') === true){
+
+                totals.invisible += 1
+        }
+      }
+
 
   // Once you've done the counting, this function will update the display
   displayTotals(totals)
